3a520630c4964f8a415bc41065802183a78e3a4f
vijayathl <vijay@healthlevel.com>
2020-06-10 22:26:31 +0530

    First commit: build

---
+++ precache-manifest.bd23780ac19637f68c98c75d3050dba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b69ae3df3000c12f25e45b6667812e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff7cdfcb2b15f5195408",
+    "url": "/static/css/main.eb9f6e6e.chunk.css"
+  },
+  {
+    "revision": "bb869d5560bb5b04e3f9",
+    "url": "/static/js/2.4aa2f51b.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/static/js/2.4aa2f51b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7cdfcb2b15f5195408",
+    "url": "/static/js/main.cb13e7ee.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  }
+]);
